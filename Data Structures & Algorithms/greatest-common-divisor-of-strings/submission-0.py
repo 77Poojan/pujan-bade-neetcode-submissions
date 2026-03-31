a5e8dbd1e1ef1df6bb5e8123ce9f8c6da12d37f5
Pujan Bade <55784329+77Poojan@users.noreply.github.com>
2026-03-31 13:25:16 +0545

    Add: greatest-common-divisor-of-strings - submission-0

---
+++ Data Structures & Algorithms/greatest-common-divisor-of-strings/submission-0.py
@@ -0,0 +1,11 @@
+from math import gcd 
+
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        g = gcd(len(str1), len(str2))
+        
+
+        if all(str1[i] == str2[i % g] for i in range(len(str1))) and \
+           all(str2[i] == str2[i % g] for i in range(len(str2))):
+            return str1[:g]
+        return ""
