c914de4e257d5cfecb1f2b29a27839d4aed89dbc
Pujan Bade <55784329+77Poojan@users.noreply.github.com>
2026-04-22 14:52:09 +0545

    Add: find-in-mountain-array - submission-3

---
+++ Data Structures & Algorithms/find-in-mountain-array/submission-3.py
@@ -0,0 +1,52 @@
+class Solution:
+    def findInMountainArray(self, target: int, mountainArr: 'MountainArray') -> int:
+        n = mountainArr.length() - 1
+        if mountainArr.length() < 2:
+            return -1
+
+        l, r = 0, n
+
+        cache = {}
+
+        # Cached Getter
+        def get(i):
+            if i not in cache:
+                cache[i] = mountainArr.get(i)
+            return cache[i]
+
+        while l < r:
+            mid = (l + r) // 2
+
+            if get(mid) < get(mid + 1):
+                l = mid + 1
+            else:
+                r = mid 
+
+        def minimumIndex(l, r, hill):
+            while l <= r:
+                mid = (l + r) // 2
+                mid_val = get(mid)
+
+                if mid_val == target:
+                    return mid
+
+                if hill:
+                    if mid_val < target:
+                        l = mid + 1
+                    else:
+                        r = mid - 1
+                else:
+                    if mid_val < target:
+                        r = mid - 1
+                    else:
+                        l = mid + 1
+            return -1
+
+
+        ## Ascend to mountain
+        idx = minimumIndex(0, l, True)
+        if idx != -1:
+            return idx
+
+        ## Descend to mountain
+        return minimumIndex(l + 1, n, False)      
