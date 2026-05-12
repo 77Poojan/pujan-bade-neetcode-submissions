ee09774f5b05682230af5c46b62810cae697da31
Pujan Bade <55784329+77Poojan@users.noreply.github.com>
2026-05-12 14:24:31 +0545

    Add: extra-characters-in-a-string - submission-4

---
+++ Data Structures & Algorithms/extra-characters-in-a-string/submission-4.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        trie = {}
+
+        def insert(word: str) -> None:
+            t = trie
+            for ch in word:
+                if ch not in t:
+                    t[ch] = {}
+                t = t[ch]
+            t["#"] = True
+
+        for d in dictionary:
+            insert(d)
+
+        n = len(s)
+        dp = [0] * (n + 1)
+
+        for i in range(n - 1, -1, -1):  
+            dp[i] = 1 + dp[i + 1]
+            curr = trie
+            for j in range(i, n):
+                ch = s[j]
+                if ch not in curr:
+                    break
+                curr = curr[ch]
+                if "#" in curr:
+                    dp[i] = min(dp[i], dp[j + 1])
+
+        return dp[0]
+        
