37ab3c264af85bf1989a453c0566cba8847d9b31
Pujan Bade <55784329+77Poojan@users.noreply.github.com>
2026-05-21 14:22:53 +0545

    Add: longest-happy-string - submission-3

---
+++ Data Structures & Algorithms/longest-happy-string/submission-3.py
@@ -0,0 +1,29 @@
+class Solution:
+    def longestDiverseString(self, a: int, b: int, c: int) -> str:
+        counter = {"a": a, "b": b, "c": c}
+        heap = [(-v, k) for k, v in counter.items() if v > 0]
+        heapq.heapify(heap)
+
+        res = []
+        while heap:
+            count1, ch1 = heapq.heappop(heap)
+
+            if (len(res) >= 2) and (res[-1] == res[-2] == ch1):
+                if not heap:
+                    break
+
+                count2, ch2 = heapq.heappop(heap)
+                res.append(ch2)
+
+                if count2 + 1 < 0:
+                    heapq.heappush(heap, (count2 + 1, ch2))
+                
+                heapq.heappush(heap, (count1, ch1))
+
+            else:
+                res.append(ch1)
+                if count1 + 1 < 0:
+                    heapq.heappush(heap, (count1 + 1, ch1))
+                
+
+        return "".join(res)
