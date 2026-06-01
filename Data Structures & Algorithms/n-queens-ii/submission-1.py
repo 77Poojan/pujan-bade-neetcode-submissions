d374f7549a67a697c8b6c376a1b83776d3b305b7
Pujan Bade <55784329+77Poojan@users.noreply.github.com>
2026-06-01 13:47:44 +0545

    Add: n-queens-ii - submission-1

---
+++ Data Structures & Algorithms/n-queens-ii/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        col = set()
+        posDiag = set()
+        negDiag = set()
+
+        self.count = 0
+        board = [["."] * n for _ in range(n)]
+
+        def backtrack(r):
+            if r == n:
+                self.count += 1
+                return
+
+            for c in range(n):
+                if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                    continue
+
+                col.add(c)
+                posDiag.add(r + c)
+                negDiag.add(r - c)
+                board[r][c] = "Q"
+                
+                backtrack(r + 1)
+
+                col.remove(c)
+                posDiag.remove(r + c)
+                negDiag.remove(r - c)
+                board[r][c] = "."
+            
+        backtrack(0)
+        return self.count
