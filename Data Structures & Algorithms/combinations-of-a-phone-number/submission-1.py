6f7bafa740d0caaacb496803458e88380d686111
Pujan Bade <55784329+77Poojan@users.noreply.github.com>
2026-06-01 12:14:31 +0545

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        res, sol = [], []
+        telepad = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+
+        def backtrack(i):
+            if i == len(digits):
+                if sol:
+                    res.append("".join(sol[:]))
+                return
+
+            for ch in telepad[digits[i]]:
+                sol.append(ch)
+                backtrack(i + 1)
+                sol.pop()
+
+        backtrack(0)
+        return res
