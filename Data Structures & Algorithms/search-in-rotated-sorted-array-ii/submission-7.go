20d6b94497b9aa9dfae0fd2aae40c58f78d342d4
Pujan Bade <55784329+77Poojan@users.noreply.github.com>
2026-08-21 23:29:17 +0545

    Add: search-in-rotated-sorted-array-ii - submission-7

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-7.go
@@ -0,0 +1,29 @@
+func search(nums []int, target int) bool {
+    l := 0
+    r := len(nums) - 1
+
+    for l <= r {
+        mid := l + (r - l) / 2
+
+        if nums[mid] == target {
+            return true
+        } else if nums[l] == nums[mid] && nums[mid] == nums[r] {
+            l += 1
+            r -= 1
+        } else if nums[l] <= nums[mid] {
+            if nums[l] <= target && target < nums[mid] {
+                r = mid - 1
+            } else {
+                l = mid + 1
+            }
+        } else {
+            if nums[mid] < target && target <= nums[r] {
+                l = mid + 1
+            } else {
+                r = mid - 1
+            }
+        }
+    }
+
+    return false 
+}
