782a797de325dd49299700cafc93f0f26144b6d0
Pujan Bade <55784329+77Poojan@users.noreply.github.com>
2026-08-25 00:42:26 +0545

    Add: permutation-string - submission-9

---
+++ Data Structures & Algorithms/permutation-string/submission-9.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        dp1 = [0] * 26
+        dp2 = [0] * 26
+
+        for i in range(len(s1)):
+            dp1[ord(s1[i]) - 97] += 1
+            dp2[ord(s2[i]) - 97] += 1
+
+        if dp1 == dp2:
+            return True
+
+        for j in range(len(s1), len(s2)):
+            dp2[ord(s2[j]) - 97] += 1
+            dp2[ord(s2[j - len(s1)]) - 97] -= 1
+
+            if dp1 == dp2:
+                return True
+            
+        return False
