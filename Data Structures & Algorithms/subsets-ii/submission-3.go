239bc8f897d6c705f6dcbce6c8113b987330dc59
Pujan Bade <55784329+77Poojan@users.noreply.github.com>
2026-09-02 23:13:10 +0545

    Add: subsets-ii - submission-3

---
+++ Data Structures & Algorithms/subsets-ii/submission-3.go
@@ -0,0 +1,23 @@
+func subsetsWithDup(nums []int) [][]int {
+	sort.Ints(nums)
+	sol := []int {} 
+	res := [][]int {}
+
+	var backtrack func(i int) 
+
+	backtrack = func(i int) {
+		res = append(res, append([]int{}, sol...))
+
+		for j := i; j < len(nums); j++ {
+			if j > i && nums[j] == nums[j - 1] {
+				continue
+			}
+			sol = append(sol, nums[j])
+			backtrack(j + 1)
+			sol = sol[:len(sol) - 1]
+		}
+	}
+
+	backtrack(0)
+	return res
+}
