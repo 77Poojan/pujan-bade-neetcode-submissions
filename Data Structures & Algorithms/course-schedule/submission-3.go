148545a9cc42644cbeb87ba975384b1cdefeb82b
Pujan Bade <55784329+77Poojan@users.noreply.github.com>
2026-09-17 12:10:54 +0545

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.go
@@ -0,0 +1,42 @@
+func canFinish(numCourses int, prerequisites [][]int) bool {
+    const (
+        UNVISITED = 0
+        VISITING  = 1
+        VISITED   = 2
+    )
+
+    graph := make([][]int, numCourses)
+    for _, p := range prerequisites {
+        u, v := p[0], p[1]
+        graph[u] = append(graph[u], v)
+    }
+
+    track := make([]int, numCourses)
+
+    var dfs func(node int) bool
+    dfs = func(node int) bool {
+        if track[node] == VISITING {
+            return false
+        }
+        if track[node] == VISITED {
+            return true
+        }
+
+        track[node] = VISITING
+        for _, pre := range graph[node] {
+            if !dfs(pre) {
+                return false
+            }
+        }
+        track[node] = VISITED
+        return true
+    }
+
+    for i := 0; i < numCourses; i++ {
+        if !dfs(i) {
+            return false
+        }
+    }
+
+    return true
+}
